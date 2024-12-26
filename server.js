bb8caf72b9fb4efb612bd0e961b9a90a98a58e5f
soulstar9792 <secondstar9792@gmail.com>
2024-12-27 05:38:43 +0900

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
