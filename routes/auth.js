fac2bc3de8576531b6ad75dd4aa59ffb23d6f92e
soulstar9792 <secondstar9792@gmail.com>
2024-12-29 00:35:50 +0900

    get_users api error handling

--- routes/auth.js
+++ routes/auth.js
@@ -59,6 +59,7 @@ router.post('/login', async (req, res) => {
 router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find();
+        console.log(users);
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({ error: 'Server error' });
