c9c5d3b9b1d10fd3c8c5f98b173acc5f1acbc93f
soulstar9792 <secondstar9792@gmail.com>
2024-12-28 12:51:10 +0900

    console debug added

--- server.js
+++ server.js
@@ -15,16 +15,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api/auth', authRouter);
+console.log("--------------");
+console.log("--------------");
+console.log("HELLO WORLD");
+console.log("--------------");
+console.log("--------------");
+
 
 mongoose.connect(process.env.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));;
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log('connected to db');
+  console.log('Db opened');
 });
 
 app.get('/', (req, res) => {
