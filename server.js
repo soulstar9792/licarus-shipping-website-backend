852235599af68435985d1a857323a2e639ffd761
Wishorn <secondstar9792@gmail.com>
2025-03-08 11:24:32 +0800

    Add Socket Server

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// Socket Server
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const SocketServer = require('./SocketServer');
+const sServer = new SocketServer(firebaseConfig, 8080); // You can change the port if needed
+
+// API Server
 const authRouter = require("./routes/auth");
 const OrderRouter = require("./routes/OrderLabel");
 const paymentRouter = require("./routes/payment");
