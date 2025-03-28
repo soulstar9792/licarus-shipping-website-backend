ea951f40f70c0b013d17bb9dddfbfa0c04773342
Wishorn <wishorn@ship.com>
2025-03-28 10:20:20 +0800

    getInvoice function added to btcPayAction.js

--- utils/btcPayActions.js
+++ utils/btcPayActions.js
@@ -50,6 +50,28 @@ const BTCPayServerAPI = {
 
     return await response.json();
   },
+
+  async getInvoice(storeId, invoiceId) {
+    const fetch = (await import('node-fetch')).default; // Dynamic import
+
+    const apiEndpoint = `/api/v1/stores/${storeId}/invoices/${invoiceId}`;
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: 'token ' + process.env.BTCPAY_API_KEY,
+    };
+
+    const response = await fetch(process.env.BTCPAY_SERVER_URL + apiEndpoint, {
+      method: 'GET',
+      headers: headers,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`BTCPay API Error: ${errorData}`);
+    }
+
+    return await response.json();
+  },
 };
 
 module.exports = BTCPayServerAPI; // Exporting the API
